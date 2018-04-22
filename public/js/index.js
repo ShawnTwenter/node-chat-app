5074c3496fd711b47a21e87eadbab85967d8a892
Shawn Twenter <shawn.twenter@storage-mart.com>
2018-04-22 13:35:29 -0500

    emit new message on create message

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,6 @@
 var socket = io();
 socket.on('connect', function()  {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Shawn',
-        text: 'Yup, that works for me.'
-    });
 });
 
 
--- server/server.js
+++ server/server.js
@@ -15,16 +15,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New User Connected');
 
-   
-
-    socket.emit('newMessage', {
-        from: 'Ted',
-        text: 'hey, wazzzupp',
-        createAt: 123
-    });
 
     socket.on('createMessage',(message) => {
         console.log('Create Message: ',message);
+        io.emit('newMessage', {
+            from: message.from ,
+            text: message.text,
+            createAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
